047f600ad88606ade5553e0c5e023e4164616002
Lorenzo Garbarino <garbarino65@gmail.com>
2025-10-11 18:29:45 +0200

    Ho "sistemato" il codice del filto, ho tolto la matrice dei coefficenti che non sapendo dove metterla dava fastidio alla compilazione, ho aggiunto i codici di data maker,sink,clock e la tb che ci ha dato il prof ma non ho ancora fatto nessun controllo sulla compatibilità

--- lab1/hdl_2.1/FIR.vhd
+++ lab1/hdl_2.1/FIR.vhd
@@ -3,14 +3,15 @@ use ieee.std_logic_1164.all;
 use ieee.std_logic_arith.all;
 use ieee.std_logic_unsigned.all;
 
+
 ENTITY FIR is
-	
-	type Coeff_mat is array(0 to 9) of unsigned(10 downto 0);
-	PORT(DIN: IN unsigned (10 downto 0);
-	     Coeff: IN Coeff_mat;
-	     VIN,RSTn: in std_logic;
+--type Coeff_mat is array(0 to 9) of unsigned(10 downto 0); 
+	PORT(
+		DIN: IN unsigned (10 downto 0);
+	    Coeff: IN unsigned(120 downto 0);
+	     VIN,RSTn,clk: in std_logic;
 	     DOUT: out unsigned(10 downto 0);
-	     VOUT: out std_logic;
+	     VOUT: out std_logic
 	);
 	
 
@@ -18,11 +19,11 @@ END FIR;
 
 ARCHITECTURE struct of FIR is
 	
-	COMPONENT Adder
-		PORT(IN0, IN1: IN UNSIGNED(10 downto 0);
-             	     Sum: OUT UNSIGNED(10 downto 0)
-			);
-	END COMPONENT;
+	--COMPONENT Adder
+	--	PORT(IN0, IN1: IN UNSIGNED(10 downto 0);
+      --       	     Sum: OUT UNSIGNED(10 downto 0)
+		--	);
+	--END COMPONENT;
 
         COMPONENT Mult
 		PORT(IN0, IN1: IN UNSIGNED(10 downto 0);
@@ -35,7 +36,7 @@ ARCHITECTURE struct of FIR is
                      Din: IN UNSIGNED(10 downto 0);
 	             Dout: OUT UNSIGNED(10 downto 0)     
 	     		);
-	END COMPONENT
+	END COMPONENT;
 
 	COMPONENT AddMultBlock
 		PORT(DIN, Coeff, DINadd0: IN UNSIGNED (10 downto 0);
@@ -47,9 +48,9 @@ ARCHITECTURE struct of FIR is
 
 
 	type ADDout_arr is array(0 to 8) of unsigned(10 downto 0);
-        type Dout_reg_arr is array (1 to 9) of unsigned(10 downto 0);
+    type Dout_reg_arr is array (1 to 9) of unsigned(10 downto 0);
 
-	signal DoutReg: DIN_arr;
+	signal DoutReg: Dout_reg_arr;
 	signal ADDout: Addout_arr;
 	signal Data_reg0_out, Coeff0_out, Add0_in: unsigned(9 downto 0);
 	signal mult_out: unsigned (21 downto 0);
@@ -61,26 +62,27 @@ ARCHITECTURE struct of FIR is
 	VOUT <= VIN;
 
 	filter: for i in 0 to 9 generate
-	   filter_block_0: if i = 0 generate
-		Data_reg0: REG11B port map(clk,VIN,RSTn,DIN,Data_reg0_out);
-		Coeff_reg0: REG11B port map(clk,VIN,RSTn,Coeff(0),Coeff0_out);
-		mult0: mult port map(Data_reg0_out,Coeff0_out,mult_out);
+	    
+		filter_block_0: if i = 0 generate
+			Data_reg0: REG11B port map(clk,VIN,RSTn,DIN,Data_reg0_out);
+			Coeff_reg0: REG11B port map(clk,VIN,RSTn,Coeff((120-i) downto (110-i)),Coeff0_out);
+			mult0: mult port map(Data_reg0_out,Coeff0_out,mult_out);
 
 		Add0_in <= "00" & mult_out(21 downto 13);
            end generate filter_block_0;
 
 	   filter_block_1: if i = 1 generate
-                first_cell: AddMultBlock port map(Data_reg0_out,Coeff(1),Add0_in,CLK,RSTn,VIN,Addout(0),DoutReg(1));
+                first_cell: AddMultBlock port map(Data_reg0_out,Coeff((120-i) downto (110-i)),Add0_in,CLK,RSTn,VIN,Addout(0),DoutReg(1));
 		
 	   end generate filter_block_1;
 
 	   filter_block_i: if i > 1 generate
-		cell_i: AddMultBlock port map(DoutReg(i-1),Coeff(i),Addout(i-2),CLK,RSTn,VIN,Addout(i-1),DoutReg(i));
+		cell_i: AddMultBlock port map(DoutReg(i-1),Coeff((120-i) downto (110-i)),Addout(i-2),CLK,RSTn,VIN,Addout(i-1),DoutReg(i));
 	   end generate filter_block_i;
 	
         end generate filter;
 
-        output_register: REG11B port map(clk,VIN,RSTn,Addout(8),DOUT);
+    output_register: REG11B port map(clk,VIN,RSTn,Addout(8),DOUT);
 
 	
 END struct;
