75b7f43ab99bebd4366060ab4112c2445974ea5a
Giangix02 <s341166@studenti.polito.it>
2025-10-13 17:16:38 +0200

    push nuove cartelle + file

---
+++ lab1/hdl_2.1/V2/AddMultBlock.vhd
@@ -0,0 +1,53 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_arith.all;
+use ieee.std_logic_signed.all;
+
+ENTITY AddMultBlock is
+
+	PORT(DIN, Coeff, DINadd0: IN SIGNED (10 downto 0);
+	     CLK,RSTn, Vin: IN std_logic;
+             Dout,DATA_REG: OUT signed (10 downto 0) --DATA_REG is the actual pipelined data
+             );
+
+END AddMultBlock;
+
+ARCHITECTURE beh of AddMultBlock is
+	
+	COMPONENT Adder
+		PORT(IN0, IN1: IN SIGNED(10 downto 0);
+             	     Sum: OUT SIGNED(10 downto 0)
+			);
+	END COMPONENT;
+
+        COMPONENT Mult
+		PORT(IN0, IN1: IN SIGNED(10 downto 0);
+             	     Molt: OUT SIGNED(21 downto 0)
+			);
+        END COMPONENT;
+
+	COMPONENT REG11B 
+		PORT(CLK,EN,RSTn: IN STD_LOGIC;
+                     Din: IN SIGNED(10 downto 0);
+	             Dout: OUT SIGNED(10 downto 0)     
+	     		);
+	END COMPONENT;
+
+	SIGNAL INMUL1, INMUL0, DINadd1: SIGNED (10 downto 0);
+	SIGNAL MUL_OUT: SIGNED (21 downto 0);
+	
+
+	begin
+
+	DATA_REG <= INMUL0;
+
+	DINadd1 <= MUL_OUT(21) & MUL_OUT(21) & MUL_OUT(21 downto 13);
+	Addereeno: Adder port map(DINadd0, DINadd1, Dout); 
+	Multiplier: Mult port map(INMUL0, INMUL1, MUL_OUT);
+	Reg_data: REG11B port map(CLK,Vin,RSTn,DIN,INMUL0);
+	Reg_coeff: REG11B port map(CLK,Vin,RSTn,Coeff,INMUL1);
+	
+
+
+END beh;
+
--- lab1/hdl_2.1/V2/FIRV2.vhd
+++ lab1/hdl_2.1/V2/FIRV2.vhd
@@ -3,18 +3,18 @@ use ieee.std_logic_1164.all;
 use ieee.std_logic_arith.all;
 use ieee.std_logic_unsigned.all;
 
-ENTITY FIRV3 is
+ENTITY FIRV2 is
 	
-	PORT(DIN,C0,C1,C2,C3,C4,C5,C6,C7,C8,C9,C10: IN signed (10 downto 0);
+	PORT(DIN,C0,C1,C2,C3,C4,C5,C6,C7,C8,C9,C10: IN unsigned (10 downto 0);
 	     VIN,RSTn,clk: in std_logic;
 	     DOUT: out unsigned(10 downto 0);
 	     VOUT: out std_logic
 	);
 	
 
-END FIRV3;
+END FIRV2;
 
-ARCHITECTURE struct of FIRV3 is
+ARCHITECTURE struct of FIRV2 is
 	
 	--COMPONENT Adder
 		--PORT(IN0, IN1: IN UNSIGNED(10 downto 0);
@@ -35,7 +35,7 @@ ARCHITECTURE struct of FIRV3 is
 	     		);
 	END COMPONENT;
 
-	COMPONENT AddMultBlockV2
+	COMPONENT AddMultBlock
 		PORT(DIN, Coeff, DINadd0: IN UNSIGNED (10 downto 0);
 	     	     CLK,RSTn, Vin: IN std_logic;
                      Dout, DATA_REG: OUT unsigned (10 downto 0)
@@ -61,22 +61,22 @@ ARCHITECTURE struct of FIRV3 is
        --cell0 (different from the others)
 
 	Data_reg0: REG11B port map(clk,VIN,RSTn,DIN,DoutReg(0));
-
-	mult0: mult port map(DoutReg(0),C0,mult_out);
+	Coeff_reg0: REG11B port map(clk,VIN,RSTn,C0,Coeff0_out);
+	mult0: mult port map(DoutReg(0),Coeff0_out,mult_out);
 	Add0_in <= "00" & mult_out(21 downto 13);
 
      
 
-    cell1:      AddMultBlockV2 port map(DoutReg(0),C1,Add0_in,CLK,RSTn,VIN,Addout(0),DoutReg(1));
-	cell2: 	    AddMultBlockV2 port map(DoutReg(1),C2,Addout(0),CLK,RSTn,VIN,Addout(1),DoutReg(2));
-	cell3: 	    AddMultBlockV2 port map(DoutReg(1),C3,Addout(1),CLK,RSTn,VIN,Addout(2),DoutReg(3));
-	cell4: 	    AddMultBlockV2 port map(DoutReg(1),C4,Addout(2),CLK,RSTn,VIN,Addout(3),DoutReg(4));
-	cell5: 	    AddMultBlockV2 port map(DoutReg(1),C5,Addout(3),CLK,RSTn,VIN,Addout(4),DoutReg(5));
-	cell6: 	    AddMultBlockV2 port map(DoutReg(1),C6,Addout(4),CLK,RSTn,VIN,Addout(5),DoutReg(6));
-	cell7: 	    AddMultBlockV2 port map(DoutReg(1),C7,Addout(5),CLK,RSTn,VIN,Addout(6),DoutReg(7));
-	cell8: 	    AddMultBlockV2 port map(DoutReg(1),C8,Addout(6),CLK,RSTn,VIN,Addout(7),DoutReg(8));
-	cell9: 	    AddMultBlockV2 port map(DoutReg(1),C9,Addout(7),CLK,RSTn,VIN,Addout(8),DoutReg(9));
-	cell10: 	AddMultBlockV2 port map(DoutReg(1),C10,Addout(8),CLK,RSTn,VIN,Addout(9),DoutReg(10));
+    cell1:      AddMultBlock port map(DoutReg(0),C1,Add0_in,CLK,RSTn,VIN,Addout(0),DoutReg(1));
+	cell2: 	    AddMultBlock port map(DoutReg(1),C2,Addout(0),CLK,RSTn,VIN,Addout(1),DoutReg(2));
+	cell3: 	    AddMultBlock port map(DoutReg(1),C3,Addout(1),CLK,RSTn,VIN,Addout(2),DoutReg(3));
+	cell4: 	    AddMultBlock port map(DoutReg(1),C4,Addout(2),CLK,RSTn,VIN,Addout(3),DoutReg(4));
+	cell5: 	    AddMultBlock port map(DoutReg(1),C5,Addout(3),CLK,RSTn,VIN,Addout(4),DoutReg(5));
+	cell6: 	    AddMultBlock port map(DoutReg(1),C6,Addout(4),CLK,RSTn,VIN,Addout(5),DoutReg(6));
+	cell7: 	    AddMultBlock port map(DoutReg(1),C7,Addout(5),CLK,RSTn,VIN,Addout(6),DoutReg(7));
+	cell8: 	    AddMultBlock port map(DoutReg(1),C8,Addout(6),CLK,RSTn,VIN,Addout(7),DoutReg(8));
+	cell9: 	    AddMultBlock port map(DoutReg(1),C9,Addout(7),CLK,RSTn,VIN,Addout(8),DoutReg(9));
+	cell10: 	AddMultBlock port map(DoutReg(1),C10,Addout(8),CLK,RSTn,VIN,Addout(9),DoutReg(10));
 	
         output_register: REG11B port map(clk,VIN,RSTn,Addout(8),DOUT);
 
