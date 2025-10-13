7a06319493f09c4ac32338cdb22deaa9cc030c3d
Lorenzo Garbarino <garbarino65@gmail.com>
2025-10-13 17:32:53 +0200

    modificata la tb delle celle

--- lab1/hdl_2.1/AddMultBlock_tb.vhd
+++ lab1/hdl_2.1/AddMultBlock_tb.vhd
@@ -27,7 +27,7 @@ component AddMultBlock
 
 END component;
 
-signal din_tb, cf_tb, dout_tb, datarg_tb, in0_tb, mul_in_tb: unsigned(10 downto 0);
+signal din_tb, cf_tb, dout_tb, datarg_tb, in0_tb, mul_in_tb,cf1_tb,dout1_tb,datarg1_tb: unsigned(10 downto 0);
 signal mul_out_tb: unsigned(21 downto 0);
 signal clk_tb, rst_tb, vin_tb : std_logic;
 
@@ -46,6 +46,8 @@ begin
 rst_tb <= '0';
 din_tb <= unsigned(conv_std_logic_vector(0, 11));
 cf_tb <= unsigned(conv_std_logic_vector(-13, 11));
+cf1_tb <= unsigned(conv_std_logic_vector(-26, 11));
+vin_tb <= '0';
 wait for 300 ns;
 rst_tb <= '1';
 wait for 100 ns;
@@ -55,11 +57,11 @@ wait;
 end process;
 
 in0_tb <= unsigned(conv_std_logic_vector(-1, 11));
-mul_in_tb <= "00" & mul_out_tb(21 downto 13);
+mul_in_tb <=mul_out_tb(21 downto 13) & "00";
 
 moltiplicatore : Mult port map (din_tb, in0_tb, mul_out_tb);
 
 cell : AddMultBlock port map (din_tb, cf_tb, mul_in_tb, clk_tb, rst_tb, vin_tb, dout_tb, datarg_tb);
-cell1 : AddMultBlock port map (din1_tb, cf1_tb, mul1_in_tb, clk_tb, rst_tb, vin_tb, dout1_tb, datarg1_tb);
+cell1 : AddMultBlock port map (datarg_tb, cf1_tb, dout_tb, clk_tb, rst_tb, vin_tb, dout1_tb, datarg1_tb);
 
 end behavior;
