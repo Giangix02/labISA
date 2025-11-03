3633982cf0cfecb77626218a4d00bc293584fcae
Giangix02 <notgiangix@gmail.com>
2025-11-03 16:27:02 +0100

    non ricordo, qualcosa che c'entra col placeroute

--- lab1/FIRunfolded/sim/script.sh
+++ lab1/FIRunfolded/sim/script.sh
@@ -9,12 +9,12 @@ vcom -work ./work ../V1/FF.vhd
 vcom -work ./work ../V1/REG11B.vhd
 vcom -work ./work ../V1/data_sink.vhd
 vcom -work ./work ../V1/data_maker.vhd
-#vcom -work ./work ../V1/FIR_UNF.vhd
+vcom -work ./work ../V1/FIR_UNF.vhd
 vcom -work ./work ../V1/clk_gen.vhd
-vlog -work ./work ../netlist/FIR_UNF.v
+#vlog -work ./work ../netlist/FIR_UNF.v
 vlog -sv -work ./work ../V1/tb_fir.sv
-vsim -L /eda/dk/nangate45/verilog/qsim2020.4 -sdftyp /tb_fir/UUT=../netlist/FIR_UNF.sdf work.tb_fir
-#vsim -L /eda/dk/nangate45/verilog/qsim2020.4 work.tb_fir
+vsim -L /eda/dk/nangate45/verilog/qsim2020.4 work.tb_fir
+#vsim -L /eda/dk/nangate45/verilog/qsim2020.4 -sdftyp /tb_fir/UUT=../netlist/FIR_UNF.sdf work.tb_fir
 
 
 
