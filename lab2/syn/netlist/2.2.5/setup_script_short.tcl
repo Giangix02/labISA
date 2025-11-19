ef638e9eead6037a18162069f09611f7e4fe010d
Giangix02 <notgiangix@gmail.com>
2025-11-19 15:37:03 +0100

    ?

--- lab2/syn/netlist/2.2.5/setup_script_short.tcl
+++ lab2/syn/netlist/2.2.5/setup_script_short.tcl
@@ -1,6 +1,6 @@
 analyze -library WORK -format sverilog {codes/cf_math_pkg.sv codes/lzc.sv codes/rr_arb_tree.sv codes/fpnew_pkg.sv codes/fpnew_classifier.sv codes/fpnew_rounding.sv codes/fpnew_fma.sv codes/fpnew_opgroup_fmt_slice.sv codes/fpnew_opgroup_block.sv codes/fpnew_top.sv}
 elaborate fpnew_top -architecture verilog -lib work
-create_clock -name MY_CLK -period 10 clk_i
+create_clock -name MY_CLK -period 2.5 clk_i
 set_dont_touch_network MY_CLK
 set_clock_uncertainty 0.07 [get_clocks MY_CLK]
 set_input_delay 0.5 -max -clock MY_CLK [remove_from_collection [all_inputs] MY_CLK]
