64d83d61e096a70ef7e56e93d27c51c6d87807a4
Lorenzo Garbarino <garbarino65@gmail.com>
2025-11-24 17:23:11 +0100

    Update Adder4b.sv

--- lab2/Multiplier/Adder4b.sv
+++ lab2/Multiplier/Adder4b.sv
@@ -5,7 +5,8 @@ module Adder4b
   output logic Cout);
   
   logic c0,c1,c2,c3;
+  FA fa0 (A[0],B[0],Cin,c0,s[0])
   FA fa1 (A[1],B[1],c0,c1,s[1]);
   FA fa2 (A[2],B[2],c1,c2,s[2]);
   FA fa3 (A[3],B[3],c2,Cout,s[3]);
- endmodule 
+ endmodule
