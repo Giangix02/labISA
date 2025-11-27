e7d134a478e13606e3fe4e302d5dded19ccd414b
Lorenzo Garbarino <garbarino65@gmail.com>
2025-11-27 16:25:21 +0100

    aggiunta tb nuovi moltiplicatore

--- lab2/Multiplier/tb_MantMult.sv
+++ lab2/Multiplier/tb_MantMult.sv
@@ -2,25 +2,28 @@
 module tb_MantMult ();
 
 logic[7:0] A_tb,B_tb = '0;
-logic[15:0] M_tb;
+logic[15:0] M_tb,M_ideal,diff;
 
 MantissaMultiplier UUT(A_tb,B_tb,M_tb);
+assign M_ideal = A_tb*B_tb;
+assign diff = A - B;
 initial begin 
+$monitor("The results different is: %d,diff);
 #10;
-A_tb = 13;
-B_tb = 10;
+A_tb = $urandom%255;
+B_tb = $urandom%255;
 #20
-A_tb = 118;
-B_tb = 35;
+A_tb = $urandom%255;
+B_tb = $urandom%255;
 #30;
-A_tb = 224;
-B_tb = 224;
+A_tb = $urandom%255;
+B_tb = $urandom%255;
 #40;
-A_tb = 255;
-B_tb = 255;
+A_tb = $urandom%255;
+B_tb = $urandom%255;
 #50;
-A_tb = 255;
-B_tb = 0;
+A_tb = $urandom%255;
+B_tb = $urandom%255;
 end
 endmodule
 
